709865604b37a053e5e8f3280d2525bddfc7c2b6
Ori Nachum <ori.nachum@gmail.com>
2025-05-03 10:58:11 +0000

    chore(tests): responses test

---
+++ test_tools/call_responses_proxy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+curl localhost:8080/responses \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $OPENAI_API_KEY" \
+  -d '{
+    "model": "meta-llama/llama-4-scout-17b-16e-instruct",
+    "input": "Say 3 words, and 3 words only.",
+    "max_output_tokens": 100,
+    "temperature": 0.7,
+    "stream": true
+  }'

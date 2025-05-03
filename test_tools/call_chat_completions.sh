68aaeaf7b2baaa296186034ad5a44760289e7d71
Ori Nachum <ori.nachum@gmail.com>
2025-05-03 02:33:12 +0000

    chore(tests): test tool for validating secrets

---
+++ test_tools/call_chat_completions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+curl ${OPENAI_BASE_URL_INTERNAL}/chat/completions -s \
+-H "Content-Type: application/json" \
+-H "Authorization: Bearer ${OPENAI_API_KEY}" \
+-d '{
+"model": "meta-llama/llama-4-scout-17b-16e-instruct",
+"messages": [{
+    "role": "user",
+    "content": "Say 3 words, and 3 words only."
+}]
+}'

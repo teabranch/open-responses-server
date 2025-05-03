10778399aecb1a1240aedd52efd0d6b390e2e38b
Ori Nachum <ori.nachum@gmail.com>
2025-05-03 12:18:54 +0000

    chore(tests): add testing tools get models and set up codex

--- test_tools/call_codex.sh
+++ test_tools/call_codex.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
-
+nvm install 22.15.0
 npm install -g @openai/codex
 
-curl localhost:8080/responses \
-  -H "Content-Type: application/json" \
-  -H "Authorization: Bearer $OPENAI_API_KEY" \
-  -d '{
-    "model": "meta-llama/llama-4-scout-17b-16e-instruct",
-    "input": "Say 3 words, and 3 words only.",
-    "max_output_tokens": 100,
-    "temperature": 0.7,
-    "stream": true
-  }'
+codex
---
+++ test_tools/call_models_proxy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl localhost:8080/models -s \
+-H "Content-Type: application/json" \
+-H "Authorization: Bearer ${OPENAI_API_KEY}"

7d2854eda4f3d5d490280b245f61dad28ea9710e
Ori Nachum <ori.nachum@gmail.com>
2025-05-14 11:42:44 +0000

    chore(doc): add documentation for main module

---
+++ docs/prompts/document-server.py
@@ -0,0 +1,4 @@
+Explain server.py so I understand deeply how it works.
+Don't make it deeply technical - make it high level, main methods, and give me the base of understanding to be able to refactor it.
+Give extra attention around tools and functions_calls
+Write it all in docs/open-responses-server.md

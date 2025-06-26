a8f4bca2ab51537a61108d6083e69b372cc0e902
Ori Nachum <ori.nachum@gmail.com>
2025-06-26 09:00:38 +0000

    save demo chat completions

--- demo/chat_completions.sh
+++ demo/chat_completions.sh
@@ -3,6 +3,9 @@
 source "$(dirname "$0")/config.sh"
 
 echo "Calling MCP proxy for chat completions with model: ${MODEL_ID}"
+echo ""
+echo "User: Just call 'list_tables' action"
+echo "..."
 # save as variable then print it
 response=$(curl localhost:8080/v1/chat/completions -s \
 -H "Content-Type: application/json" \
@@ -14,4 +17,7 @@ response=$(curl localhost:8080/v1/chat/completions -s \
     "content": "Just call 'list_tables' action"
 }]
 }')
-echo "$response"
+# {"id":"chatcmpl-BmcEY89U4SIv18BI1LENuuSY6stq4","object":"chat.completion","created":1750926414,"model":"gpt-4o-2024-08-06","choices":[{"index":0,"message":{"role":"assistant","content":"It seems that there is a single table named `listings` in the database.","refusal":null,"annotations":[]},"logprobs":null,"finish_reason":"stop"}],"usage":{"prompt_tokens":230,"completion_tokens":18,"total_tokens":248,"prompt_tokens_details":{"cached_tokens":0,"audio_tokens":0},"completion_tokens_details":{"reasoning_tokens":0,"audio_tokens":0,"accepted_prediction_tokens":0,"rejected_prediction_tokens":0}},"service_tier":"default","system_fingerprint":"fp_07871e2ad8"}
+# I just want the text content of the response
+response=$(echo "$response" | jq -r '.choices[0].message.content')
+echo "AI Response: $response"
